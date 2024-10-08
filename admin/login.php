0bd17663a888264cda22a5f06b619c2e54b2b94b
Faculty Loading <cosemichelle2@gmail.com>
2024-10-08 18:04:49 +0800

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -145,7 +145,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                                         <input type="checkbox" name="remember">Remember Me
                                     </label>
                                     <label>
-                                        <a href="forgotpassword.php">Forgot Password?</a>
+                                        <a href=https://github.com/misyel15/TABODI/blob/main/admin/forgotpassword.php>Forgot Password?</a>
                                     </label>
                                 </div>
                                 <button class="au-btn au-btn--block au-btn--blue m-b-20" type="submit">Login</button>
