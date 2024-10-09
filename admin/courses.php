feb3b5b4d16e24a217d239c9cb4988a1f183762a
Faculty Loading <cosemichelle2@gmail.com>
2024-10-09 21:18:07 +0800

    Update courses.php

--- admin/courses.php
+++ admin/courses.php
@@ -1,25 +1,36 @@
-<?php 
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
 include("db_connect.php");
+
+// Check database connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Include necessary files
 include 'includes/style.php'; 
 include 'includes/head.php'; 
-$error="";
-$msg="";
+$error = "";
+$msg = "";
 
+// Use PHPMailer classes
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+// Load PHPMailer classes
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
 require 'phpmailer/src/SMTP.php';
 
-function sendemail($email, $reset_token)
-{
+// Function to send email
+function sendemail($email, $reset_token) {
     $mail = new PHPMailer(true);
 
     try {
-        //Server settings
+        // Server settings
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
@@ -28,14 +39,14 @@ function sendemail($email, $reset_token)
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->Port = 465;
 
-        //Recipients
+        // Recipients
         $mail->setFrom('mccschedsystem@gmail.com', 'MCC SCHED SYSTEM ADMIN');
         $mail->addAddress($email);
 
-        //Reset link
+        // Reset link
         $resetLink = 'https://mccfacultyscheduling.com/admin/reset_password.php?email=' . urlencode($email) . '&token=' . $reset_token;
 
-        //Content
+        // Content
         $mail->isHTML(true);
         $mail->Subject = 'Here is your link to Reset the password of your MCC SCHED-SYSTEM Account';
         $mail->Body = "
@@ -77,10 +88,11 @@ function sendemail($email, $reset_token)
         $mail->send();
         return true;
     } catch (Exception $e) {
-        return false;
+        return false; // Optionally log error message: $mail->ErrorInfo
     }
 }
 
+// Handle form submission
 if (isset($_POST['reset'])) {
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $check = "SELECT * FROM users WHERE email = '$email'";
@@ -124,33 +136,30 @@ if (isset($_POST['reset'])) {
     }
 }
 ?>
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-      <!-- Required meta tags-->
+    <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="School Faculty Scheduling System">
     <meta name="author" content="Your Name">
     <meta name="keywords" content="School, Faculty, Scheduling, System">
 
-    <!-- Title Page-->
+    <!-- Title Page -->
     <title>Login</title>
     <link rel="icon" href="assets/uploads/mcclogo.jpg" type="image/jpg">
-    <!-- Fontfaces CSS-->
+    <!-- Fontfaces CSS -->
     <link href="css/font-face.css" rel="stylesheet" media="all">
     <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
     <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
     <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
 
-    <!-- Bootstrap CSS-->
+    <!-- Bootstrap CSS -->
     <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
 
-    <!-- Vendor CSS-->
+    <!-- Vendor CSS -->
     <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
     <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
     <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
@@ -159,7 +168,7 @@ ini_set('display_errors', 1);
     <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
     <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
 
-    <!-- Main CSS-->
+    <!-- Main CSS -->
     <link href="css/theme.css" rel="stylesheet" media="all">
 
     <!-- Include SweetAlert CSS -->
@@ -245,50 +254,27 @@ ini_set('display_errors', 1);
         </div>
         <div class="card-body">
             <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
-            <form action="" method="post">
+            <form method="POST" action="">
                 <div class="input-group mb-3">
                     <input type="email" name="email" class="form-control" placeholder="Email" required>
                     <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-envelope"></span>
-                        </div>
+                        <div class="input-group-text"><i class="fa fa-envelope"></i></div>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-8">
                         <button type="submit" name="reset" class="btn btn-primary btn-block">Request new password</button>
+                    </div>
+                    <div class="col-4">
+                        <a href="login.php" class="btn btn-secondary btn-block">Back</a>
                     </div>
                 </div>
             </form>
-            <p class="mt-3 mb-1">
-                <a href="index.php">Login</a>
-            </p>
         </div>
     </div>
 </div>
 
-    <!-- Jquery JS-->
-    <script src="vendor/jquery-3.2.1.min.js"></script>
-    <!-- Bootstrap JS-->
-    <script src="vendor/bootstrap-4.1/popper.min.js"></script>
-    <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
-    <!-- Vendor JS -->
-    <script src="vendor/slick/slick.min.js"></script>
-    <script src="vendor/wow/wow.min.js"></script>
-    <script src="vendor/animsition/animsition.min.js"></script>
-    <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
-    <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
-    <script src="vendor/counter-up/jquery.counterup.min.js"></script>
-    <script src="vendor/circle-progress/circle-progress.min.js"></script>
-    <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="vendor/chartjs/Chart.bundle.min.js"></script>
-    <script src="vendor/select2/select2.min.js"></script>
-
-    <!-- Include SweetAlert JS -->
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.all.min.js"></script>
-
-    <!-- Main JS-->
-    <script src="js/main.js"></script>
-
-
+<!-- Include SweetAlert JS -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+</body>
 </html>
