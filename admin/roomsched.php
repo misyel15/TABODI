3cfe26f0484ccd5d742cc9d50989e37dc2fd0ccd
Faculty Loading <cosemichelle2@gmail.com>
2024-10-14 06:26:14 +0800

    Update roomsched.php

--- admin/roomsched.php
+++ admin/roomsched.php
@@ -62,7 +62,7 @@ if (isset($_POST['room_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Schedule Load</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  
     <style>
         @media (max-width: 768px) {
             .card-header {
