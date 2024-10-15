bbc112568499194eaa74b8e8586544a0cbd7a1bf
Faculty Loading <cosemichelle2@gmail.com>
2024-10-15 20:46:37 +0800

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -8,7 +8,6 @@ if (!isset($_SESSION['username']) || !isset($_SESSION['dept_id'])) {
     exit();
 }
 
-include 'includes/header.php'; 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -78,6 +77,8 @@ include 'includes/header.php';
             }
         }
     </style>
+     <?php include 'notif.php';
+          include 'includes/header.php';  ?>
 </head>
 <body>
     <div class="container main-container" style="margin-top:100px;">
