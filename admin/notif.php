2e135bebea76cb1e1ccba1c0c2e8797a5d05dd8b
Faculty Loading <cosemichelle2@gmail.com>
2024-10-15 20:31:43 +0800

    Update notif.php

--- admin/notif.php
+++ admin/notif.php
@@ -1,13 +1,21 @@
 <?php
-    include 'include/config.php';          
+    include 'db_connect.php';          
 
     // Count the number of unread notifications
     $unreadQuery = "SELECT COUNT(*) AS unread_count FROM notifications WHERE status = 'unread'";
-    $unreadResult = mysqli_query($bd, $unreadQuery);
+    $unreadResult = mysqli_query($conn, $unreadQuery);
+
+    if (!$unreadResult) {
+        die('Error with unread notifications query: ' . mysqli_error($conn));
+    }
+
     $unreadData = mysqli_fetch_assoc($unreadResult);
-    $unreadCount = $unreadData['unread_count'];
+    $unreadCount = isset($unreadData['unread_count']) ? $unreadData['unread_count'] : 0;
 
     // Fetch all notifications
-    $rt = mysqli_query($bd, "SELECT * FROM notifications ORDER BY timestamp DESC");
-    $num1 = mysqli_num_rows($rt);
-    ?>
+    $rt = mysqli_query($conn, "SELECT * FROM notifications ORDER BY timestamp DESC");
+
+    if (!$rt) {
+        die('Error fetching notifications: ' . mysqli_error($conn));
+    }
+?>
