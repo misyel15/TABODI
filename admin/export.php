2f0fc192da0900879f492a41fb287d1882f2666a
Faculty Loading <cosemichelle2@gmail.com>
2024-10-15 10:55:15 +0800

    Update export.php

--- admin/export.php
+++ admin/export.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include('db_connect.php');
-include 'includes/header.php';
+
 
 // Assuming you store the department ID in the session during login
 // Example: $_SESSION['dept_id'] = $user['dept_id'];
