d6052464198f7fc981eac8168405ac34a154a956
Faculty Loading <cosemichelle2@gmail.com>
2024-10-21 12:41:54 +0800

    Update home.php

--- home.php
+++ home.php
@@ -86,12 +86,11 @@
 <body>
 <main id="main" class="bg-light">
   <div id="login" class="col-md-4">
-    <div class="card">
-      
-      <a href="login.php" class="close" aria-label="Close" onclick="this.closest('.card').remove();">
-          <span aria-hidden="true">&times;</span>
-        </a><div class="card-body">
-        
+    <div class="card position-relative"> <!-- Added position-relative -->
+      <a href="login.php" class="close" aria-label="Close" onclick="this.closest('.card').remove();" style="position: absolute; top: 10px; right: 10px;"> <!-- Inline styles for positioning -->
+        <span aria-hidden="true">&times;</span>
+      </a>
+      <div class="card-body">
         <form id="login-form">
           <h4><b>Welcome To Faculty Scheduling System</b></h4>
           <div class="form-group">
@@ -106,6 +105,7 @@
     </div>
   </div>
 </main>
+
 
 
 
