57d095ad66306bb6374562eb323c1daadb4e036e
Faculty Loading <cosemichelle2@gmail.com>
2024-10-21 11:51:56 +0800

    Update home.php

--- home.php
+++ home.php
@@ -10,31 +10,52 @@
   <!-- Include Bootstrap for styling -->
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
   <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
-    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+  <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+  <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
 
   <?php include('./header.php'); ?>
 
   <?php 
   if(isset($_SESSION['login_id']))
-  header("location:index.php");
+    header("location:index.php");
   ?>
 
 </head>
 <style>
-  body{
+  body {
     width: 100%;
     height: calc(100%);
-    position:fixed;
+    position: fixed;
+    margin: 0; /* Remove default body margin */
   }
-  #main{
+  
+  #main {
     width: calc(100%);
     height: calc(100%);
-    display:flex;
-    align-items:center;
-    justify-content:center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative; /* Required for positioning the overlay */
+    overflow: hidden; /* Prevent overflow */
   }
-  #login{
+  
+  #main::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url('your-image-url.jpg'); /* Replace with your image URL */
+    background-size: cover; /* Cover the entire section */
+    background-position: center; /* Center the image */
+    opacity: 0.5; /* Adjust transparency (0.0 to 1.0) */
+    z-index: 1; /* Place it behind the content */
+  }
+
+  #login {
+    position: relative; /* Required to place it above the background */
+    z-index: 2; /* Place it above the overlay */
   }
   
   .navbar-brand {
@@ -49,12 +70,9 @@
 
   <!-- Navigation Bar -->
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">
-  <img src="mcclogo.jpg" alt="Logo" style="width: 40px; height: 30px; margin-right: 10px;">MCC Faculty Scheduling System
-</a>
-
-
-   
+    <a class="navbar-brand" href="#">
+      <img src="mcclogo.jpg" alt="Logo" style="width: 40px; height: 30px; margin-right: 10px;">MCC Faculty Scheduling System
+    </a>
   </nav>
 
   <main id="main" class="bg-dark">
@@ -67,7 +85,7 @@
               <label for="id_no" class="control-label">Please enter your Faculty ID No.</label>
               <input type="text" id="id_no" name="id_no" class="form-control">
             </div>
-            <center><button class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></center>
+            <center><button type="submit" class="btn-sm btn-block btn-wave col-md-4 btn-primary">Login</button></center>
           </form>
           <br>
         </div>
@@ -85,26 +103,27 @@
   <script>
     $('#login-form').submit(function(e){
       e.preventDefault();
-      $('#login-form button[type="button"]').attr('disabled',true).html('Logging in...');
-      if($(this).find('.alert-danger').length > 0 )
+      $('#login-form button[type="submit"]').attr('disabled', true).html('Logging in...');
+      if ($(this).find('.alert-danger').length > 0) {
         $(this).find('.alert-danger').remove();
+      }
       $.ajax({
-        url:'admin/ajax.php?action=login_faculty',
-        method:'POST',
-        data:$(this).serialize(),
-        error:err=>{
+        url: 'admin/ajax.php?action=login_faculty',
+        method: 'POST',
+        data: $(this).serialize(),
+        error: err => {
           console.log(err);
-          $('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+          $('#login-form button[type="submit"]').removeAttr('disabled').html('Login');
         },
-        success:function(resp){
-          if(resp == 1){
-            location.href ='index.php';
-          }else{
+        success: function(resp) {
+          if (resp == 1) {
+            location.href = 'index.php';
+          } else {
             $('#login-form').prepend('<div class="alert alert-danger">ID Number is incorrect.</div>');
-            $('#login-form button[type="button"]').removeAttr('disabled').html('Login');
+            $('#login-form button[type="submit"]').removeAttr('disabled').html('Login');
           }
         }
-      })
+      });
     });
   </script>
 
